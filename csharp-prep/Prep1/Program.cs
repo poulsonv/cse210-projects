5b7db21f4929d5e636fcc54a1a4f1d94e7ef0076
patrickarmani <arm22001@byui.edu>
2023-01-07 14:38:11 -0300

    Learn Activity 01 - c# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Learning Activity - C# Prep 1
+        //Write a program that asks for your name
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {last_name}, {name} {last_name}!");
+        
     }
 }
