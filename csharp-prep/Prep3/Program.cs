ba2d89536e86b7e6a4f50ab726ff14531c74630a
patrickarmani <arm22001@byui.edu>
2023-01-12 00:49:44 -0300

    "Learning Activity - C# Prep 3"

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        // Parts 1 and 2, the user give the number
+        
+        //Console.Write("What is the magic number? ");
+        //string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        // Part 3 - random njumber
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+        
     }
 }
