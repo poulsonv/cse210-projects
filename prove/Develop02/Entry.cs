644526679c14d01563c4ad653a29ddd472554c62
patrickarmani <arm22001@byui.edu>
2023-02-09 21:51:03 -0300

    entry finish

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,32 @@
+using System; 
+
+class Entry
+
+
+{
+    public List<string> questions = new List<string> ()
+    {
+        "Who was the most interesting person I interacted with today? ",
+        "What was the best part of my day? ",
+        "How did I see the hand of the Lord in my life today? ",
+        "What was the strongest emotion I felt today? ",
+        "If I had one thing I could do over today, what would it be? ",
+    };
+    public void QuestionOption()  
+    {
+        foreach (string question in questions)
+        {
+            Console.WriteLine(question);
+        }
+    }
+
+    public string RandomQuestions() 
+    {
+        Random random = new Random();
+        int question = random.Next(questions.Count);
+        return questions[question]; 
+    }
+
+
+
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,69 +1,57 @@
 using System;
-
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Write your name and last name:  ");
-        string _givenName = Console.ReadLine();
+        Console.WriteLine();// line space
+        // The Journal initializy with title
+        Console.WriteLine("DIGITAL JOURNAL");
+        // line space
+        Console.WriteLine();
+        // The user write entry dates
+        Console.WriteLine("Please, choose an option: ");
 
-        Console.WriteLine("Writer Locate: (City , State , Country ");
-        string _givenPlace = Console.ReadLine();
-
-        DateTime dataEntry = DateTime.Now;
-
-        Console.WriteLine($"{_givenName}, {_givenPlace}.");
-        Console.WriteLine($"Day {dataEntry}.");
-        
-    }
-}
-public class Journal
-{
-    static void ShowUserAnswer(string[] args)
-    {
-        Console.WriteLine("What devious do you face today? ");
-        string _givenQuestionOne = Console.ReadLine();
-
-        Console.WriteLine($"My main challenges today have been {_givenQuestionOne}.");
-
-        Console.WriteLine("What have you done to overcome this challenge? ");
-        string _givenQuestionTwo = Console.ReadLine();
-
-        Console.WriteLine($"To overcome this challenge I {_givenQuestionTwo}.");
-
-    }
-}
-
-public class End
-{
-    static void ShowUserScripture(string[] args)
-    {
-        Console.WriteLine("Name a scripture you read or studied today? (mention the reference - ex: 1 Nephi 3:7) ");
-        string _askEscripture  = Console.ReadLine();
-        string _askReference =  Console.ReadLine();
-
-        Console.WriteLine($"A scripture I read today is {_askEscripture}, and the reference is {_askReference}");
-    }
-
-}
-
-public class Save
-{
-    static void SaveFile(bool[] args)
-    {   
-        Console.WriteLine("Did you write the reference? ");
-        string _referenceScripute = Console.ReadLine();
-        if (_referenceScripute == "yes")
+        List<string> menuOptions = new List<string> ()
         {
-            Console.WriteLine("Your document is saved");
-        }
-        else
-        {
-            Console.WriteLine("Please quote a scripture reference.");
-        }
+            "1 - Write",
+            "2 - Display",
+            "3 - Load",
+            "4 - Save",
+            "5 - Quit",
+        };
 
 
-    }
+        string userAnswer = ""; 
     
+
+        while  (userAnswer != "5"){
+            foreach(string option in menuOptions){
+            //this code pass a list in a loop for showing every option in a different line
+            Console.WriteLine(option);
+            };
+            Console.Write("What option did you like? ");
+            userAnswer = Console.ReadLine();
+
+            if (userAnswer == "5")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bye!"); 
+                break; 
+            }
+
+            else if (userAnswer == "1") 
+            {
+                Entry entry1 = new Entry(); 
+                string option = entry1.RandomQuestions();
+                Console.Write(option);
+                userAnswer = Console.ReadLine();
+            }
+        
+        }  
+       
+
+    }
+
 }
